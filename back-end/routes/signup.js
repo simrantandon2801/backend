76db6c6233fd43c254eaea2a7ef5cf4839c18a99
simrantandon2801 <93216735+simrantandon2801@users.noreply.github.com>
2023-04-01 16:15:05 +0530

    Update signup.js

--- back-end/routes/signup.js
+++ back-end/routes/signup.js
@@ -1,21 +1,32 @@
 const router = require('express').Router();
 const bcrypt = require("bcrypt");
 const Joi = require("joi");
-const {User}=require('./models/user');
-const validate = (data) => {
+const {User,validate}=require('./models/user');
+const validate1 = (data) => {
 	const schema = Joi.object({
 		email: Joi.string().email().required().label("Email"),
 		password: Joi.string().required().label("Password"),
 	});
 	return schema.validate(data);
 };
+// const validate1 = (data) => {
+// 	const schema = Joi.object({
+// 		firstName: Joi.string().required().label("First Name"),
+// 		lastName: Joi.string().required().label("Last Name"),
+// 		email: Joi.string().email().required().label("Email"),
+//         password: passwordComplexity().required().label("Password"),
+// 	});
+// 	return schema.validate(data);
+// };
 router.post('/signup',async(req,res)=>{
     try {
+		console.log(req.body)
 		const { error } = validate(req.body);
 		if (error)
 			return res.status(400).send({ message: error.details[0].message });
 
 		const user = await User.findOne({ email: req.body.email }); 
+		console.log(user)
 		if (user)
 			return res
 				.status(409)
@@ -23,7 +34,7 @@ router.post('/signup',async(req,res)=>{
 
 		const salt = await bcrypt.genSalt(Number(process.env.SALT));
 		const hashPassword = await bcrypt.hash(req.body.password, salt);
-
+console.log(user)
 		await new User({ ...req.body, password: hashPassword }).save();
         res.status(201).send({ message: "User created successfully" });
     } catch (error) {
@@ -32,7 +43,7 @@ router.post('/signup',async(req,res)=>{
 });
 router.post('/login',async(req,res) => {
     try {
-		const { error } = validate(req.body);
+		const { error } = validate1(req.body);
 		if (error)
 			return res.status(400).send({ message: error.details[0].message });
 
